51b4eedecc2631695ab84b301f8131818ff9e2d4
Nicholas MacDonald <macdonaldn@ufl.edu>
2023-09-02 08:39:03 -0400

    add game timer functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { useTimer } from "./util/customHooks";
 
 export default function App() {
   const [showModal, setShowModal] = useState(false);
+  const [bestTime, setBestTime] = useState(false);
+  const [previousTime, setPreviousTime] = useState(false);
 
   const {
     time,
@@ -27,14 +29,29 @@ export default function App() {
     <>
       <Header
         // add time, bestTime, previousTime props
+        time={time}
+        bestTime={bestTime}
+        previousTime={previousTime}
         openModal={() => setShowModal(true)}
       />
       <CardGame
         // add onGameStart, onGameEnd props
+        onGameStart={() => {
+          timerStart();
+        }}
+        onGameEnd={() => {
+          timerStop();
+          if (time < bestTime || !(bestTime)){
+            setBestTime(time);
+          }
+          setPreviousTime(time);
+          timerReset();
+        }}
         cardTexts={cardTexts}
       />
       <Modal isShown={showModal} close={() => setShowModal(false)} />
     </>
   );
 }
+
 
